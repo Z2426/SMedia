4441db39f81489134fc38ed9ee400f8183feabd9
leeanhvu <leeanhvu404@gmail.com>
2023-10-19 16:19:15 +0700

    create connectdb file

---
+++ server/dbConfig/index.js
@@ -0,0 +1,14 @@
+import mongoose  from "mongoose";
+const dbConection = async()=>{
+    try{
+        const connect =await mongoose.connect(process.env.MONGODB_URL,{
+            useNewURLParser :true,
+            useUnifliedTopology:true
+
+        })
+        console.log("DB connected succesfully")
+    }catch(e){
+        console.log("DB error"+e)
+    }
+}
+export default  dbConection()
